a5dca55e1a71566df0a5756bdee6d88997af4002
Ana <ana.s.queiroz@ba.estudante.senai.br>
2026-09-17 12:27:53 -0300

    chore: conexão com o supabase

---
+++ Config/configuration.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DB_NAME",     "postgres");
+define("DB_USER",     "postgres.mceyqzdvwduqdilmggoc");
+define("DB_PASSWORD", "ana123sandrine@");
+define("DB_HOST",     "aws-0-ca-central-1.pooler.supabase.com");
+define("DB_PORT",     "6543");
