6b630173d3a3c622bfb3d586118fd5acedefbe13
eduardorsouza-glitch <eduardo.r.souza@ba.estudante.senai.br>
2026-09-22 08:54:12 -0300

    fix: banco

--- banco.sql
+++ banco.sql
@@ -1,9 +1,9 @@
 CREATE TABLE medias_escolares (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     aluno VARCHAR(100) NOT NULL,
-    nota1 DECIMAL(4,2) NOT NULL,
-    nota2 DECIMAL(4,2) NOT NULL,
-    nota3 DECIMAL(4,2) NOT NULL,
-    media DECIMAL(4,2) NOT NULL,
+    nota1 NUMERIC(4,2) NOT NULL,
+    nota2 NUMERIC(4,2) NOT NULL,
+    nota3 NUMERIC(4,2) NOT NULL,
+    media NUMERIC(4,2) NOT NULL,
     situacao VARCHAR(20) NOT NULL
 );
